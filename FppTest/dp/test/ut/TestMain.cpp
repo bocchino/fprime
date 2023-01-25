6e223d737eacababe572e2d21c9c8973d1f0b47d
bocchino <bocchino@jpl.nasa.gov>
2023-01-24 16:15:41 -0800

    Add dp unit tests to FppTest

---
+++ FppTest/dp/test/ut/TestMain.cpp
@@ -0,0 +1,24 @@
+// ----------------------------------------------------------------------
+// TestMain.cpp
+// ----------------------------------------------------------------------
+
+#include "STest/Random/Random.hpp"
+#include "FppTest/dp/test/ut/Tester.hpp"
+
+using namespace FppTest;
+
+TEST(schedIn, OK) {
+    Tester tester;
+    tester.schedIn_OK();
+}
+
+TEST(productRecvIn, Container1_OK) {
+    Tester tester;
+    tester.productRecvIn_Container1_OK();
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    STest::Random::seed();
+    return RUN_ALL_TESTS();
+}
---
+++ FppTest/dp/test/ut/Tester.hpp
@@ -0,0 +1,107 @@
+// ======================================================================
+// \title  DpTest/test/ut/Tester.hpp
+// \author bocchino
+// \brief  hpp file for DpTest test harness implementation class
+// ======================================================================
+
+#ifndef FppTest_DpTest_Tester_HPP
+#define FppTest_DpTest_Tester_HPP
+
+#include "Fw/Dp/test/util/DpContainerHeader.hpp"
+#include "GTestBase.hpp"
+#include "STest/Pick/Pick.hpp"
+#include "FppTest/dp/DpTest.hpp"
+
+namespace FppTest {
+
+class Tester : public DpTestGTestBase {
+    // ----------------------------------------------------------------------
+    // Construction and destruction
+    // ----------------------------------------------------------------------
+
+  public:
+    // Maximum size of histories storing events, telemetry, and port outputs
+    static constexpr FwSizeType MAX_HISTORY_SIZE = 10;
+    // Instance ID supplied to the component instance under test
+    static constexpr FwSizeType TEST_INSTANCE_ID = 0;
+    // Queue depth supplied to component instance under test
+    static constexpr FwSizeType TEST_INSTANCE_QUEUE_DEPTH = 10;
+    // The component id base
+    static constexpr FwDpIdType ID_BASE = 100;
+
+    //! Construct object Tester
+    //!
+    Tester();
+
+    //! Destroy object Tester
+    //!
+    ~Tester();
+
+  public:
+    // ----------------------------------------------------------------------
+    // Tests
+    // ----------------------------------------------------------------------
+
+    //! schedIn OK
+    void schedIn_OK();
+
+    //! productRecvIn with Container 1 OK
+    void productRecvIn_Container1_OK();
+
+  PRIVATE:
+    // ----------------------------------------------------------------------
+    // Handlers for typed from ports
+    // ----------------------------------------------------------------------
+
+    //! Handler for from_productRequestOut
+    //!
+    void from_productRequestOut_handler(const NATIVE_INT_TYPE portNum,  //!< The port number
+                                        FwDpIdType id,                  //!< The container ID
+                                        FwDpBuffSizeType size           //!< The size of the requested buffer
+    );
+
+    //! Handler for from_productSendOut
+    //!
+    void from_productSendOut_handler(const NATIVE_INT_TYPE portNum,  //!< The port number
+                                     FwDpIdType id,                  //!< The container ID
+                                     const Fw::Buffer& buffer        //!< The buffer
+    );
+
+  PRIVATE:
+    // ----------------------------------------------------------------------
+    // Helper methods
+    // ----------------------------------------------------------------------
+
+    //! Connect ports
+    //!
+    void connectPorts();
+
+    //! Initialize components
+    //!
+    void initComponents();
+
+  PRIVATE:
+    // ----------------------------------------------------------------------
+    // Variables
+    // ----------------------------------------------------------------------
+
+    //! The component under test
+    //!
+    DpTest component;
+
+    //! Buffer data for Container 1
+    U8 container1Data[DpTest::CONTAINER_1_SIZE];
+
+    //! Buffer for Container 1
+    const Fw::Buffer container1Buffer;
+
+    //! Buffer data for Container 2
+    U8 container2Data[DpTest::CONTAINER_2_SIZE];
+
+    //! Buffer for Container 2
+    const Fw::Buffer container2Buffer;
+};
+
+}  // end namespace FppTest
+
+#endif
