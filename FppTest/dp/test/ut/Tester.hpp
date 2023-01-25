47fcb43c85aa5cc67a94bfee4e36a04d5e9db082
bocchino <bocchino@jpl.nasa.gov>
2023-01-24 17:27:42 -0800

    Revise dp tests

--- FppTest/dp/test/ut/Tester.hpp
+++ FppTest/dp/test/ut/Tester.hpp
@@ -7,10 +7,10 @@
 #ifndef FppTest_DpTest_Tester_HPP
 #define FppTest_DpTest_Tester_HPP
 
+#include "FppTest/dp/DpTest.hpp"
 #include "Fw/Dp/test/util/DpContainerHeader.hpp"
 #include "GTestBase.hpp"
 #include "STest/Pick/Pick.hpp"
-#include "FppTest/dp/DpTest.hpp"
 
 namespace FppTest {
 
@@ -45,11 +45,90 @@ class Tester : public DpTestGTestBase {
     //! schedIn OK
     void schedIn_OK();
 
+    void productRecvIn_InvokeAndCheckHeader(FwDpIdType id,
+                                            FwSizeType dataEltSize,
+                                            Fw::Buffer& buffer,
+                                            FwSizeType& expectedNumElts) {
+        const auto containerId = ID_BASE + DpTest::ContainerId::Container1;
+        // Set the test time
+        const Fw::Time timeTag = this->randomizeTestTime();
+        // Invoke the productRecvIn port
+        this->invoke_to_productRecvIn(0, containerId, this->container1Buffer);
+        this->component.doDispatch();
+        // Check the port history size
+        ASSERT_FROM_PORT_HISTORY_SIZE(1);
+        ASSERT_from_productSendOut_SIZE(1);
+        // Get the history entry
+        const auto entry = this->fromPortHistory_productSendOut->at(0);
+        // Check the container id
+        ASSERT_EQ(entry.id, containerId);
+        // Check the buffer size
+        buffer = entry.buffer;
+        const auto bufferSize = buffer.getSize();
+        ASSERT_GE(bufferSize, FwDpBuffSizeType(DpTest::DpContainer::Header::SIZE));
+        // Deserialize the packet header
+        Fw::TestUtil::DpContainerHeader header;
+        header.deserialize(buffer);
+        // Check the container id
+        ASSERT_EQ(header.id, containerId);
+        // Check the priority
+        ASSERT_EQ(header.priority, DpTest_Priority::Container1);
+        // Check the time tag
+        ASSERT_EQ(header.timeTag, timeTag);
+        // Check the data size
+        const auto dataCapacity = bufferSize - DpTest::DpContainer::Header::SIZE;
+        const auto eltSize = sizeof(FwDpIdType) + dataEltSize;
+        expectedNumElts = dataCapacity / eltSize;
+        const auto expectedDataSize = expectedNumElts * eltSize;
+        ASSERT_EQ(header.dataSize, expectedDataSize);
+        // Check the buffer size
+        const auto expectedBufferSize = DpTest::DpContainer::Header::SIZE + expectedDataSize;
+        ASSERT_EQ(bufferSize, expectedBufferSize);
+    }
+
     //! productRecvIn with Container 1 OK
-    void productRecvIn_Container1_OK();
+    void productRecvIn_Container1_OK() {
+        Fw::Buffer buffer;
+        FwSizeType expectedNumElts;
+        // Invoke the port and check the header
+        this->productRecvIn_InvokeAndCheckHeader(DpTest::ContainerId::Container1, sizeof(U32), buffer, expectedNumElts);
+        // Check the data
+        auto& serialRepr = buffer.getSerializeRepr();
+        for (FwDpBuffSizeType i = 0; i < expectedNumElts; ++i) {
+            FwDpIdType id;
+            U32 elt;
+            auto status = serialRepr.deserialize(id);
+            ASSERT_EQ(status, Fw::FW_SERIALIZE_OK);
+            const FwDpIdType expectedId = this->component.getIdBase() + DpTest::RecordId::U32Record;
+            ASSERT_EQ(id, expectedId);
+            status = serialRepr.deserialize(elt);
+            ASSERT_EQ(status, Fw::FW_SERIALIZE_OK);
+            ASSERT_EQ(elt, this->component.u32RecordData);
+        }
+    }
 
     //! productRecvIn with Container 2 OK
-    void productRecvIn_Container2_OK();
+    void productRecvIn_Container2_OK() {
+        Fw::Buffer buffer;
+        FwSizeType expectedNumElts;
+        // Invoke the port and check the header
+        this->productRecvIn_InvokeAndCheckHeader(DpTest::ContainerId::Container2, DpTest_Data::SERIALIZED_SIZE, buffer, expectedNumElts);
+#if 0
+    // Check the data
+    auto& serialRepr = entryBuffer.getSerializeRepr();
+    for (FwDpBuffSizeType i = 0; i < expectedNumElts; ++i) {
+        FwDpIdType id;
+        U32 elt;
+        auto status = serialRepr.deserialize(id);
+        ASSERT_EQ(status, Fw::FW_SERIALIZE_OK);
+        const FwDpIdType expectedId = this->component.getIdBase() + DpTest::RecordId::U32Record;
+        ASSERT_EQ(id, expectedId);
+        status = serialRepr.deserialize(elt);
+        ASSERT_EQ(status, Fw::FW_SERIALIZE_OK);
+        ASSERT_EQ(elt, this->component.u32RecordData);
+    }
+#endif
+    }
 
   PRIVATE:
     // ----------------------------------------------------------------------
