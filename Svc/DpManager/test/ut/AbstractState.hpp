8f65528f23d9ab2b995672043b2201b4082909d5
bocchino <bocchino@jpl.nasa.gov>
2023-06-29 09:50:28 -0700

    Revise DpManager unit tests

--- Svc/DpManager/test/ut/AbstractState.hpp
+++ Svc/DpManager/test/ut/AbstractState.hpp
@@ -13,6 +13,8 @@
 #ifndef Svc_AbstractState_HPP
 #define Svc_AbstractState_HPP
 
+#include <cstring>
+
 #include "Fw/Types/Assert.hpp"
 #include "Svc/DpManager/DpManager.hpp"
 
@@ -35,22 +37,61 @@ class AbstractState {
     //! Change status
     enum class ChangeStatus { CHANGED, NOT_CHANGED };
 
+    //! An optional value
+    template <typename T>
+    class Option {
+      private:
+        enum class State { VALUE, NO_VALUE };
+
+      public:
+        Option<T>(T value) : state(State::VALUE), value(value) {}
+        Option<T>() : state(State::NO_VALUE) { ::memset(&this->value, 0, sizeof this->value); }
+
+      public:
+        static Option<T> some(T value) { return Option(value); }
+        static constexpr Option<T> none() { return Option(); }
+
+      public:
+        bool hasValue() const { return this->state == State::VALUE; }
+        void set(T value) {
+            this->state = State::VALUE;
+            this->value = value;
+        }
+        T get() const {
+            FW_ASSERT(this->hasValue());
+            return this->value;
+        }
+        T getOrElse(T value) const {
+            T result = value;
+            if (this->hasValue()) {
+                result = this->value;
+            }
+            return result;
+        }
+
+      private:
+        State state;
+        T value;
+    };
+
     //! A model of an on-change telemetry channel
     template <typename T>
     class OnChangeChannel {
       public:
-        OnChangeChannel(T value) : value(value), prevIsValid(false), prev(value) {}
-        ChangeStatus updatePrev() { 
-          const auto status = ((!this->prevIsValid) || (this->value != this->prev)) ?
-            ChangeStatus::CHANGED : ChangeStatus::NOT_CHANGED;
-          this->prev = this->value;
-          return status;
+        OnChangeChannel(T value) : value(value) {}
+        ChangeStatus updatePrev() {
+            const auto status = ((!this->prev.hasValue()) || (this->value != this->prev.get()))
+                                    ? ChangeStatus::CHANGED
+                                    : ChangeStatus::NOT_CHANGED;
+            this->prev.set(this->value);
+            return status;
         }
+
       public:
         T value;
-      PRIVATE:
-        bool prevIsValid;
-        T prev;
+
+      private:
+        Option<T> prev;
     };
 
   public:
