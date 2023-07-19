83d4fd6ba0bd27f002219caf2c6a5c99e0b5311e
bocchino <bocchino@jpl.nasa.gov>
2023-07-18 18:03:46 -0700

    Revise DpManager unit tests
    Add comments and requirements

--- Svc/DpManager/test/ut/TestMain.cpp
+++ Svc/DpManager/test/ut/TestMain.cpp
@@ -10,36 +10,53 @@
 namespace Svc {
 
   TEST(BufferGetStatus, Invalid) {
+    COMMENT("Set the buffer get status to INVALID.");
     BufferGetStatus::Tester tester;
     tester.Invalid();
   }
 
   TEST(BufferGetStatus, Valid) {
+    COMMENT("Set the buffer get status to VALID.");
     BufferGetStatus::Tester tester;
     tester.Valid();
   }
 
   TEST(ProductRequestIn, BufferInvalid) {
+    COMMENT("Invoke productRequestIn in a state where the test harness returns an invalid buffer.");
+    REQUIREMENT("SVC-DPMANAGER-001");
+    REQUIREMENT("SVC-DPMANAGER-003");
     ProductRequestIn::Tester tester;
     tester.BufferInvalid();
   }
 
   TEST(ProductRequestIn, BufferValid) {
+    COMMENT("Invoke productRequestIn in a state where the test harness returns a valid buffer.");
+    REQUIREMENT("SVC-DPMANAGER-001");
+    REQUIREMENT("SVC-DPMANAGER-003");
     ProductRequestIn::Tester tester;
     tester.BufferValid();
   }
 
   TEST(ProductSendIn, OK) {
+    COMMENT("Invoke productSendIn with nominal input.");
+    REQUIREMENT("SVC-DPMANAGER-002");
+    REQUIREMENT("SVC-DPMANAGER-003");
     ProductSendIn::Tester tester;
     tester.OK();
   }
 
   TEST(SchedIn, OK) {
+    COMMENT("Invoke schedIn with nominal input.");
+    REQUIREMENT("SVC-DPMANAGER-003");
     SchedIn::Tester tester;
     tester.OK();
   }
 
   TEST(Scenarios, Random) {
+    COMMENT("Random scenario with all rules.");
+    REQUIREMENT("SVC-DPMANAGER-001");
+    REQUIREMENT("SVC-DPMANAGER-002");
+    REQUIREMENT("SVC-DPMANAGER-003");
     const FwSizeType numSteps = 10000;
     Scenarios::Random::Tester tester;
     tester.run(numSteps);
