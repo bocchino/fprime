b9964988379a092b4692d5e7179c8746c5531d35
Rob Bocchino <bocchino@jpl.nasa.gov>
2024-09-17 13:34:47 -0700

    Revise SmGuard

--- FppTest/state_machine/internal/harness/SmGuard.hpp
+++ FppTest/state_machine/internal/harness/SmGuard.hpp
@@ -26,14 +26,38 @@ class SmGuard {
     //! Constructor
     SmGuard(T initialValue  //!< The initial value
             )
-        : history(initialValue) {}
+        : m_callHistory(initialValue) {}
+
+    //! Reset the guard to the initial state
+    void reset() {
+        this->m_callHistory.clear();
+        this->m_returnValue = false;
+    }
+
+    //! Clear the call history but keep the return value
+    void clearCallHistory() { this->m_callHistory.clear(); }
+
+    //! Call the guard
+    bool call(T arg) {
+        this->m_callHistory.pushElement(arg);
+        return this->m_returnValue;
+    }
+
+    //! Set the return value
+    void setReturnValue(bool returnValue  //!< The return value
+    ) {
+        this->m_returnValue = returnValue;
+    }
+
+    //! Get the call history
+    const History<T, size>& getCallHistory() const { return this->m_callHistory; }
 
   private:
     //! The return value
     bool m_returnValue = false;
 
     //! The call history
-    History m_callHistory;
+    History<T, size> m_callHistory;
 };
 
 }  // end namespace FppTest
