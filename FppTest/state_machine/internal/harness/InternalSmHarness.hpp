e078f51cad6cf9416af838865975427f9d6fcf64
Rob Bocchino <bocchino@jpl.nasa.gov>
2024-09-17 13:39:15 -0700

    Add header file for test harness

---
+++ FppTest/state_machine/internal/harness/InternalSmHarness.hpp
@@ -0,0 +1,22 @@
+// ======================================================================
+//
+// \title  InternalSmHarness.hpp
+// \author R. Bocchino
+// \brief  Header file for internal state machine test harness
+//
+// \copyright
+// Copyright 2024, by the California Institute of Technology.
+// ALL RIGHTS RESERVED. United States Government Sponsorship
+// acknowledged.
+//
+// ======================================================================
+
+#ifndef FppTest_InternalSmHarness_HPP
+#define FppTest_InternalSmHarness_HPP
+
+#include "FppTest/state_machine/internal/harness/History.hpp"
+#include "FppTest/state_machine/internal/harness/NoArgSmGuard.hpp"
+#include "FppTest/state_machine/internal/harness/NoValueHistory.hpp"
+#include "FppTest/state_machine/internal/harness/SmGuard.hpp"
+
+#endif
