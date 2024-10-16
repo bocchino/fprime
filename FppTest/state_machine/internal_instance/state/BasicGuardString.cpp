a6a6cf1147e86129372326044de8d38e54978285
Rob Bocchino <bocchino@jpl.nasa.gov>
2024-10-16 15:52:25 -0700

    Revise sm instance state tests

--- FppTest/state_machine/internal_instance/state/BasicGuardString.cpp
+++ FppTest/state_machine/internal_instance/state/BasicGuardString.cpp
@@ -71,7 +71,26 @@ void BasicGuardString::testFalse() {
 }
 
 void BasicGuardString::testTrue() {
-    // TODO
+    this->m_smStateBasicGuardString_action_a_history.clear();
+    this->m_smStateBasicGuardString_guard_g.reset();
+    this->m_smStateBasicGuardString_guard_g.setReturnValue(true);
+    this->init(queueDepth, instanceId);
+    ASSERT_EQ(this->smStateBasicGuardString_getState(), SmState_BasicGuardString::State::S);
+    ASSERT_EQ(this->m_smStateBasicGuardString_action_a_history.getSize(), 0);
+    ASSERT_EQ(this->m_smStateBasicGuardString_guard_g.getCallHistory().getSize(), 0);
+    Fw::String value;
+    SmHarness::Pick::string(value, SmState::basicGuardStringSize);
+    this->smStateBasicGuardString_sendSignal_s(value);
+    this->doDispatch();
+    ASSERT_EQ(this->smStateBasicGuardString_getState(), SmState_BasicGuardString::State::T);
+    ASSERT_EQ(this->m_smStateBasicGuardString_guard_g.getCallHistory().getSize(), 1);
+    ASSERT_EQ(this->m_smStateBasicGuardString_guard_g.getCallHistory().getSignals().getItemAt(0),
+              SmState_BasicGuardString::Signal::s);
+    ASSERT_EQ(this->m_smStateBasicGuardString_guard_g.getCallHistory().getValues().getItemAt(0), value);
+    ASSERT_EQ(this->m_smStateBasicGuardString_action_a_history.getSize(), 1);
+    ASSERT_EQ(this->m_smStateBasicGuardString_action_a_history.getSignals().getItemAt(0),
+              SmState_BasicGuardString::Signal::s);
+    ASSERT_EQ(this->m_smStateBasicGuardString_action_a_history.getValues().getItemAt(0), value);
 }
 
 }  // namespace SmInstanceState
