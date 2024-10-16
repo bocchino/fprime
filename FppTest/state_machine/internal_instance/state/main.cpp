440751892a649f3cc0742a05f121bccadab11fa6
Rob Bocchino <bocchino@jpl.nasa.gov>
2024-10-16 14:10:27 -0700

    Revise sm instance state tests

--- FppTest/state_machine/internal_instance/state/main.cpp
+++ FppTest/state_machine/internal_instance/state/main.cpp
@@ -5,11 +5,22 @@
 #include <gtest/gtest.h>
 
 #include "FppTest/state_machine/internal_instance/state/Basic.hpp"
+#include "FppTest/state_machine/internal_instance/state/BasicGuard.hpp"
 #include "STest/STest/Random/Random.hpp"
 
 TEST(Basic, Test) {
   FppTest::SmInstanceState::Basic basic("basic");
   basic.test();
+}
+
+TEST(BasicGuard, False) {
+  FppTest::SmInstanceState::BasicGuard basicGuard("basicGuard");
+  basicGuard.testFalse();
+}
+
+TEST(BasicGuard, True) {
+  FppTest::SmInstanceState::BasicGuard basicGuard("basicGuard");
+  basicGuard.testTrue();
 }
 
 // ----------------------------------------------------------------------
