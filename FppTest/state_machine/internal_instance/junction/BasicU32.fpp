9cfeadb536f3462a692a5084f1c98908a7e4b3a5
Rob Bocchino <bocchino@jpl.nasa.gov>
2024-10-21 13:54:41 -0700

    Add BasicU32 test

---
+++ FppTest/state_machine/internal_instance/junction/BasicU32.fpp
@@ -0,0 +1,13 @@
+module FppTest {
+
+  module SmInstanceJunction {
+
+    active component BasicU32 {
+
+      state machine instance smJunctionBasicU32: SmJunction.BasicU32 priority 2 block
+
+    }
+
+  }
+
+}
--- FppTest/state_machine/internal_instance/junction/main.cpp
+++ FppTest/state_machine/internal_instance/junction/main.cpp
@@ -5,6 +5,7 @@
 #include <gtest/gtest.h>
 
 #include "FppTest/state_machine/internal_instance/junction/Basic.hpp"
+#include "FppTest/state_machine/internal_instance/junction/BasicU32.hpp"
 #include "STest/STest/Random/Random.hpp"
 
 TEST(Basic, BasicTrue) {
@@ -25,6 +26,16 @@ TEST(Basic, SmJunctionBasicTrue) {
 TEST(Basic, SmJunctionBasicFalse) {
   FppTest::SmInstanceJunction::Basic basic("basic");
   basic.testSmJunctionBasicFalse();
+}
+
+TEST(BasicU32, True) {
+  FppTest::SmInstanceJunction::BasicU32 basicU32("basicU32");
+  basicU32.testTrue();
+}
+
+TEST(BasicU32, False) {
+  FppTest::SmInstanceJunction::BasicU32 basicU32("basicU32");
+  basicU32.testFalse();
 }
 
 // ----------------------------------------------------------------------
